28db2d514b67d7865a053d69a34477ac8320938b
rvesse <rvesse@dotnetrdf.org>
2012-06-20 18:31:27 +0000

    Final tidy ups to documentation files ahead of prepping a release, bumped dependency versions to stable Jena 2.7.1 releases
    git-svn-id: svn://svn.code.sf.net/p/sparql-query-bm/code/trunk@13 37c29ba0-d37e-4076-90e9-b7b408e7d1da

--- core/src/main/java/net/sf/sparql/query/benchmarking/Benchmarker.java
+++ core/src/main/java/net/sf/sparql/query/benchmarking/Benchmarker.java
@@ -41,7 +41,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 import net.sf.sparql.query.benchmarking.monitoring.CsvProgressListener;
