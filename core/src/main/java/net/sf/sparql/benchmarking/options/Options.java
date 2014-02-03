a3b9e2f140dceff6bd48876580e0a7cb3e424792
rvesse <rvesse@dotnetrdf.org>
2014-02-03 11:19:22 +0000

    Fix up javadoc
    git-svn-id: svn://svn.code.sf.net/p/sparql-query-bm/code/trunk@60 37c29ba0-d37e-4076-90e9-b7b408e7d1da

--- core/src/main/java/net/sf/sparql/benchmarking/options/Options.java
+++ core/src/main/java/net/sf/sparql/benchmarking/options/Options.java
@@ -135,52 +135,196 @@ public interface Options {
      */
     public abstract String getCustomEndpoint(String name);
 
+    /**
+     * Resets the global run order
+     * <p>
+     * Useful for runners that incorporate warmups into their runs
+     * </p>
+     */
     public abstract void resetGlobalOrder();
 
+    /**
+     * Gets the Global Run Order
+     * <p>
+     * Called elsewhere so that mix runs and query runs record what order they
+     * were run in
+     * </p>
+     * 
+     * @return Global Run Order
+     */
     public abstract long getGlobalOrder();
 
+    /**
+     * Gets the number of parallel threads used for query run evaluation
+     * 
+     * @return Number of parallel threads
+     */
     public abstract int getParallelThreads();
 
+    /**
+     * Sets the number of parallel threads used for query run evaluation
+     * 
+     * @param threads
+     *            Number of Parallel Threads
+     */
     public abstract void setParallelThreads(int threads);
 
+    /**
+     * Gets the maximum delay between queries
+     * 
+     * @return Maximum Delay in milliseconds
+     */
     public abstract int getMaxDelay();
 
+    /**
+     * Sets the maximum delay between queries
+     * 
+     * @param milliseconds
+     *            Maximum Delay in milliseconds
+     */
     public abstract void setMaxDelay(int milliseconds);
 
+    /**
+     * Gets the HTTP authenticator in use
+     * 
+     * @return HTTP authenticator
+     * @since 1.1.0
+     */
     public abstract HttpAuthenticator getAuthenticator();
 
+    /**
+     * Sets the HTTP authenticator used
+     * 
+     * @param authenticator
+     *            HTTP authenticator
+     * @since 1.1.0
+     */
     public abstract void setAuthenticator(HttpAuthenticator authenticator);
 
+    /**
+     * Gets whether the client will allow the server to return Deflate encoded
+     * responses
+     * 
+     * @return Whether Deflate encoding is allowed
+     */
     public abstract boolean getAllowDeflateEncoding();
 
+    /**
+     * Sets whether the client will allow the server to return Deflate encoded
+     * responses
+     * 
+     * @param allowed
+     *            Whether Deflate encoding is allowed
+     */
     public abstract void setAllowDeflateEncoding(boolean allowed);
 
+    /**
+     * Gets whether the client will allow the server to return GZip encoded
+     * responses
+     * 
+     * @return Whether GZip encoding is allowed
+     */
     public abstract boolean getAllowGZipEncoding();
 
+    /**
+     * Sets whether the client should allow the server to return GZip encoded
+     * responses
+     * 
+     * @param allowed
+     *            Whether GZip encoding is allowed
+     */
     public abstract void setAllowGZipEncoding(boolean allowed);
 
+    /**
+     * Gets the Results format used for CONSTRUCT/DESCRIBE queries
+     * 
+     * @return MIME Type for CONSTRUCT/DESCRUBE results
+     */
     public abstract String getResultsGraphFormat();
 
+    /**
+     * Sets the Results format used for CONSTRUCT/DESCRIBE queries
+     * 
+     * @param contentType
+     *            MIME Type for CONSTRUCT/DESCRIBE results
+     */
     public abstract void setResultsGraphFormat(String contentType);
 
+    /**
+     * Gets the Results format used for SELECT queries
+     * 
+     * @return MIME Type for SELECT results
+     */
     public abstract String getResultsSelectFormat();
 
+    /**
+     * Sets the Results format to be used for SELECT queries
+     * 
+     * @param contentType
+     *            MIME Type for SELECT results
+     */
     public abstract void setResultsSelectFormat(String contentType);
 
+    /**
+     * Gets the Results format used for ASK queries
+     * 
+     * @return MIME Type for ASK results
+     */
     public abstract String getResultsAskFormat();
 
+    /**
+     * Sets the Results format to be used for ASK queries
+     * 
+     * @param contentType
+     *            MIME Type for ASK results
+     */
     public abstract void setResultsAskFormat(String contentType);
 
+    /**
+     * Gets the timeout for queries
+     * 
+     * @return Timeout in seconds
+     */
     public abstract int getTimeout();
 
+    /**
+     * Sets the timeout for queries
+     * 
+     * @param timeout
+     *            Timeout in seconds
+     */
     public abstract void setTimeout(int timeout);
 
+    /**
+     * Gets the number of times the Query Mix will be run as a warm up prior to
+     * actual runs
+     * 
+     * @return Number of Warmup Runs
+     */
     public abstract int getWarmups();
 
+    /**
+     * Sets the number of times the Query Mix will be run as a warm up prior to
+     * actual runs
+     * 
+     * @param runs
+     *            Number of Warmup Runs
+     */
     public abstract void setWarmups(int runs);
 
+    /**
+     * Gets the number of times the Query Mix will be run
+     * 
+     * @return Number of Runs
+     */
     public abstract int getRuns();
 
+    /**
+     * Sets the number of times the Query Mix will be run
+     * 
+     * @param runs
+     *            Number of Runs
+     */
     public abstract void setRuns(int runs);
 
     /**
@@ -252,8 +396,52 @@ public interface Options {
      */
     public abstract OperationMix getOperationMix();
 
+    /**
+     * Gets whether operation order should be randomized
+     * 
+     * @return Whether operation order is random
+     */
     public abstract boolean getRandomizeOrder();
 
+    /**
+     * Sets whether operation order should be randomized
+     * 
+     * @param randomize
+     *            Whether operation order should be random
+     */
     public abstract void setRandomizeOrder(boolean randomize);
+
+    /**
+     * Sets the Halting Behaviour
+     * 
+     * @param behaviour
+     *            Halting Behaviour
+     */
+    public abstract void setHaltBehaviour(HaltBehaviour behaviour);
+
+    /**
+     * Sets Halt on Any behavior, if set to true sets Halt on Error and Halt on
+     * Timeout to true as well
+     * 
+     * @param halt
+     *            Whether any issue should cause benchmarking to abort
+     */
+    public abstract void setHaltAny(boolean halt);
+
+    /**
+     * Sets the Halt on Error behavior
+     * 
+     * @param halt
+     *            Whether an error should cause benchmarking to abort
+     */
+    public abstract void setHaltOnError(boolean halt);
+
+    /**
+     * Sets the Halt on Timeout behavior
+     * 
+     * @param halt
+     *            Whether a timeout should cause benchmarking to abort
+     */
+    public abstract void setHaltOnTimeout(boolean halt);
 
 }
--- core/src/main/java/net/sf/sparql/benchmarking/options/OptionsImpl.java
+++ core/src/main/java/net/sf/sparql/benchmarking/options/OptionsImpl.java
@@ -57,12 +57,7 @@ public class OptionsImpl implements Options {
         return executor;
     }
 
-    /**
-     * Sets the Halt on Timeout behavior
-     * 
-     * @param halt
-     *            Whether a timeout should cause benchmarking to abort
-     */
+    @Override
     public void setHaltOnTimeout(boolean halt) {
         haltOnTimeout = halt;
     }
@@ -72,12 +67,7 @@ public class OptionsImpl implements Options {
         return haltOnTimeout;
     }
 
-    /**
-     * Sets the Halt on Error behavior
-     * 
-     * @param halt
-     *            Whether an error should cause benchmarking to abort
-     */
+    @Override
     public void setHaltOnError(boolean halt) {
         haltOnError = halt;
     }
@@ -87,13 +77,7 @@ public class OptionsImpl implements Options {
         return haltOnError;
     }
 
-    /**
-     * Sets Halt on Any behavior, if set to true sets Halt on Error and Halt on
-     * Timeout to true as well
-     * 
-     * @param halt
-     *            Whether any issue should cause benchmarking to abort
-     */
+    @Override
     public void setHaltAny(boolean halt) {
         haltAny = halt;
         if (halt) {
@@ -107,12 +91,7 @@ public class OptionsImpl implements Options {
         return haltAny;
     }
 
-    /**
-     * Sets the Halting Behaviour
-     * 
-     * @param behaviour
-     *            Halting Behaviour
-     */
+    @Override
     public void setHaltBehaviour(HaltBehaviour behaviour) {
         haltBehaviour = behaviour;
     }
@@ -194,12 +173,7 @@ public class OptionsImpl implements Options {
         return Collections.unmodifiableMap(this.customEndpoints);
     }
 
-    /**
-     * Sets the number of times the Query Mix will be run
-     * 
-     * @param runs
-     *            Number of Runs
-     */
+
     @Override
     public void setRuns(int runs) {
         if (runs < 0)
@@ -207,23 +181,13 @@ public class OptionsImpl implements Options {
         this.runs = runs;
     }
 
-    /**
-     * Gets the number of times the Query Mix will be run
-     * 
-     * @return Number of Runs
-     */
+
     @Override
     public int getRuns() {
         return runs;
     }
 
-    /**
-     * Sets the number of times the Query Mix will be run as a warm up prior to
-     * actual runs
-     * 
-     * @param runs
-     *            Number of Warmup Runs
-     */
+
     @Override
     public void setWarmups(int runs) {
         if (runs <= 0)
@@ -231,176 +195,84 @@ public class OptionsImpl implements Options {
         warmups = runs;
     }
 
-    /**
-     * Gets the number of times the Query Mix will be run as a warm up prior to
-     * actual runs
-     * 
-     * @return Number of Warmup Runs
-     */
+
     @Override
     public int getWarmups() {
         return warmups;
     }
 
-    /**
-     * Sets the timeout for queries
-     * 
-     * @param timeout
-     *            Timeout in seconds
-     */
+
     @Override
     public void setTimeout(int timeout) {
         this.timeout = timeout;
     }
 
-    /**
-     * Gets the timeout for queries
-     * 
-     * @return Timeout in seconds
-     */
+
     @Override
     public int getTimeout() {
         return timeout;
     }
 
-    /**
-     * Sets the Results format to be used for ASK queries
-     * 
-     * @param contentType
-     *            MIME Type for ASK results
-     */
     @Override
     public void setResultsAskFormat(String contentType) {
         askResultsFormat = contentType;
     }
 
-    /**
-     * Gets the Results format used for ASK queries
-     * 
-     * @return MIME Type for ASK results
-     */
     @Override
     public String getResultsAskFormat() {
         return askResultsFormat;
     }
 
-    /**
-     * Sets the Results format to be used for SELECT queries
-     * 
-     * @param contentType
-     *            MIME Type for SELECT results
-     */
     @Override
     public void setResultsSelectFormat(String contentType) {
         selectResultsFormat = contentType;
     }
 
-    /**
-     * Gets the Results format used for SELECT queries
-     * 
-     * @return MIME Type for SELECT results
-     */
     @Override
     public String getResultsSelectFormat() {
         return selectResultsFormat;
     }
 
-    /**
-     * Sets the Results format used for CONSTRUCT/DESCRIBE queries
-     * 
-     * @param contentType
-     *            MIME Type for CONSTRUCT/DESCRIBE results
-     */
     @Override
     public void setResultsGraphFormat(String contentType) {
         graphResultsFormat = contentType;
     }
 
-    /**
-     * Gets the Results format used for CONSTRUCT/DESCRIBE queries
-     * 
-     * @return MIME Type for CONSTRUCT/DESCRUBE results
-     */
     @Override
     public String getResultsGraphFormat() {
         return graphResultsFormat;
     }
 
-    /**
-     * Sets whether the client should allow the server to return GZip encoded
-     * responses
-     * 
-     * @param allowed
-     *            Whether GZip encoding is allowed
-     */
     @Override
     public void setAllowGZipEncoding(boolean allowed) {
         allowGZip = allowed;
     }
 
-    /**
-     * Gets whether the client will allow the server to return GZip encoded
-     * responses
-     * 
-     * @return Whether GZip encoding is allowed
-     */
     @Override
     public boolean getAllowGZipEncoding() {
         return allowGZip;
     }
 
-    /**
-     * Sets whether the client will allow the server to return Deflate encoded
-     * responses
-     * 
-     * @param allowed
-     *            Whether Deflate encoding is allowed
-     */
     @Override
     public void setAllowDeflateEncoding(boolean allowed) {
         allowDeflate = allowed;
     }
 
-    /**
-     * Gets whether the client will allow the server to return Deflate encoded
-     * responses
-     * 
-     * @return Whether Deflate encoding is allowed
-     */
     @Override
     public boolean getAllowDeflateEncoding() {
         return allowDeflate;
     }
 
-    /**
-     * Sets the HTTP authenticator used
-     * 
-     * @param authenticator
-     *            HTTP authenticator
-     * @since 1.1.0
-     */
     @Override
     public void setAuthenticator(HttpAuthenticator authenticator) {
         this.authenticator = authenticator;
     }
 
-    /**
-     * Gets the HTTP authenticator in use
-     * 
-     * @return HTTP authenticator
-     * @since 1.1.0
-     */
     @Override
     public HttpAuthenticator getAuthenticator() {
         return this.authenticator;
     }
 
-    /**
-     * Sets the maximum delay between queries
-     * 
-     * @param milliseconds
-     *            Maximum Delay in milliseconds
-     */
     @Override
     public void setMaxDelay(int milliseconds) {
         if (delay < 0)
@@ -408,22 +280,11 @@ public class OptionsImpl implements Options {
         delay = milliseconds;
     }
 
-    /**
-     * Gets the maximum delay between queries
-     * 
-     * @return Maximum Delay in milliseconds
-     */
     @Override
     public int getMaxDelay() {
         return delay;
     }
 
-    /**
-     * Sets the number of parallel threads used for query run evaluation
-     * 
-     * @param threads
-     *            Number of Parallel Threads
-     */
     @Override
     public void setParallelThreads(int threads) {
         if (threads < 1)
@@ -431,57 +292,26 @@ public class OptionsImpl implements Options {
         parallelThreads = threads;
     }
 
-    /**
-     * Gets the number of parallel threads used for query run evaluation
-     * 
-     * @return Number of parallel threads
-     */
     @Override
     public int getParallelThreads() {
         return parallelThreads;
     }
 
-    /**
-     * Gets the Global Run Order
-     * <p>
-     * Called elsewhere so that mix runs and query runs record what order they
-     * were run in
-     * </p>
-     * 
-     * @return Global Run Order
-     */
     @Override
     public long getGlobalOrder() {
         return globalOrder.incrementAndGet();
     }
 
-    /**
-     * Resets the global run order
-     * <p>
-     * Useful for runners that incorporate warmups into their runs
-     * </p>
-     */
     @Override
     public void resetGlobalOrder() {
         globalOrder.set(0);
     }
 
-    /**
-     * Sets whether operation order should be randomized
-     * 
-     * @param randomize
-     *            Whether operation order should be random
-     */
     @Override
     public void setRandomizeOrder(boolean randomize) {
         this.randomize = randomize;
     }
 
-    /**
-     * Gets whether operation order  should be randomized
-     * 
-     * @return Whether operation order is random
-     */
     @Override
     public boolean getRandomizeOrder() {
         return randomize;
