a2dff5625877179be759d7c9b61727800c2f9114
rvesse <rvesse@dotnetrdf.org>
2014-03-27 14:48:09 +0000

    Fix bug in benchmark.bat script

--- cmd/benchmark.bat
+++ cmd/benchmark.bat
@@ -1,1 +1,1 @@
-java %JAVA_OPTIONS% -cp "%CLASSPATH%;target/sparql-query-bm-cli.jar" net.sf.sparql.bencharmking.commands.BenchmarkCommand %*
+java %JAVA_OPTIONS% -cp "%CLASSPATH%;target/sparql-query-bm-cli.jar" net.sf.sparql.benchmarking.commands.BenchmarkCommand %*
